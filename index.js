bdc932d6394c1d60567df12af61734f7f46af53f
Matthew Linderman <mwl53@mit.edu>
2018-01-16 17:46:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians,instruments) {
   var newArray = []
-  for (var i = 0; i < musicians.length(); i++) {
+  for (var i = 0; i < musicians.length; i++) {
     var string = `${musicians[i]} plays ${instruments[i]}`
     newArray.push(string)
   }
